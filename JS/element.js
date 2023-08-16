de7d338ec5c2b3d1eedecf04ff5efd6fb27ceb8d
Yani Rachel M <98636157+yanirachel@users.noreply.github.com>
2023-08-16 11:03:47 +0530

    Add files via upload

---
+++ JS/element.js
@@ -0,0 +1,10 @@
+
+// var a=10;
+var name ='leo';
+document.write(name.length+"<br>");
+name='sim';
+name+='yani';
+document.write(name.length);
+// document.write("welcome to javascript</br>");
+// document.write("The value is",a);
+
